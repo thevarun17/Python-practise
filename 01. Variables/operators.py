baecd5887b71b0cb2dd996f2ed9d664d397e2ff7
K VARUN KUMAR <varunkumar.connect@gmail.com>
2024-10-12 19:31:04 +0530

    Add files via upload

---
+++ 01. Variables/operators.py
@@ -0,0 +1,132 @@
+####################### Operators #####################
+
+# + - * / % // **
+
+# a = 10
+# b = 3
+#
+# res = a + b
+# print(res)
+#
+# res = a - b
+# print(res)
+#
+# res = a * b
+# print(res)
+#
+# res = a / b
+# print(res)
+#
+# res = a % b
+# print(res)
+#
+# res = a // b
+# print(res)
+#
+# res = a ** b
+# print(res)
+
+
+
+# Comparison Op [ > , < , >= , <= , == , != ]
+#
+# a = 10
+# b = 10
+# res = a >= b
+# print(res)
+
+# x = "python"
+# y = "java"
+# res = x > y
+# print(res)
+
+
+# logical Op  [ and , or ,not]
+
+
+
+
+# assignment op [ += , -= ]
+
+# a = 10
+# # a = a + 5
+# a+=8
+# print(a)
+#
+# a= 10
+# # a = a - 3
+# a-=3
+# print(a)
+
+# sal = 50000
+# sal += 25000
+# print(sal)
+
+
+
+# Membership op  [ in , not in ] strings
+
+var = "python is easy to learn 1234"
+res = "python" in var
+print(res)
+
+
+var = "102569"
+res = "9" in var
+print(res)
+
+#
+#
+# pwd = "sandy@123"
+# res = " " in pwd
+# print(res)
+#
+#
+# var = """
+# kdhkldajc
+# aksdck
+# dsv'sfvdbfgbfgngfn  hfgjbjfl
+# bakjj;dlvl  b nkfjklg
+# kajj;lf  vlajkjvdf
+# nbadffklv success sdbsdk
+# sdkbsdkj
+# skdjbdsn
+# bsddv
+# sjv
+# sd
+# """
+# res = "success" in var
+# print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01. Variables/variables.py
@@ -0,0 +1,98 @@
+
+# ctrl-d it will duplicate the line
+
+
+# ctrl / is used to comment the highlighted lines
+
+
+# print("India")
+# print("Russia")
+# print("Israil")
+# print("Japan")
+# print("Iran")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------
+#  variables Chapter
+
+# a = 10    #------------integer
+# b = 20.5  # ---------------float
+# c = "Bangalore"  #--------------------string
+# d = 'Bangalore'
+# e = """ This is
+# a multi line
+# string variable
+# menas i can give more lines
+# here.
+# """
+# f = a
+# a = f
+#
+#
+# print(f)
+# print(a,b,c,d)
+# print("the value of a - " , a , " and b - ", b)
+
+# ---------------------------------
+# swapping variable with out using temp var
+
+# a = 10
+# b = "kalyan"
+# a,b = b,a
+# print("a = ", a)
+# print("b = ", b)
+# ---------------------------------
+
+## multiple variable initialization
+
+# a,b,c = "10",20,"""
+# yash wants to create multi
+# line variableboss
+# """
+#
+# print(c)
+
+# ---------------------------
+
+# Rules and regulatuions
+
+ # name = "santhosh"
+ # ssize = 46
+ # place = "Bangalore"
+ # vallet = 5000
+ # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
